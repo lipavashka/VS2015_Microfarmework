ca3ad1e977deb3ce94740e37edd01bd38d72e5db
lipavashka <lipavashka@gmail.com>
2018-03-17 12:47:56 +0200

    Add project files.

---
+++ STM32F4DISC_Test/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.SPOT;
+
+namespace STM32F4DISC_Test
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print(Resources.GetString(Resources.StringResources.String1));
+        }
+    }
+}
