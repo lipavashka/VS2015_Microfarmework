5e38492922a7e9ee794cd48cd21304e530ad2935
lipavashka <lipavashka@gmail.com>
2018-03-17 14:02:08 +0200

    Add led blink func...

--- STM32F4DISC_Test/Program.cs
+++ STM32F4DISC_Test/Program.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Threading;
 using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
 
 namespace STM32F4DISC_Test
 {
@@ -7,8 +9,24 @@ namespace STM32F4DISC_Test
     {
         public static void Main()
         {
-            Debug.Print(Resources.GetString(Resources.StringResources.String1)); // comment
+            OutputPort ledGreen = new OutputPort((Cpu.Pin)60, false);
+            OutputPort ledYellow = new OutputPort((Cpu.Pin)61, false);
+            OutputPort ledRed = new OutputPort((Cpu.Pin)62, false);
+            OutputPort ledBlue = new OutputPort((Cpu.Pin)63, false);
 
+            while (true)
+            {
+                ledGreen.Write(true);
+                ledYellow.Write(true);
+                ledRed.Write(true);
+                ledBlue.Write(true);
+                Thread.Sleep(100);
+                ledGreen.Write(false);
+                ledYellow.Write(false);
+                ledRed.Write(false);
+                ledBlue.Write(false);
+                Thread.Sleep(100);
+            }
         }
     }
 }
